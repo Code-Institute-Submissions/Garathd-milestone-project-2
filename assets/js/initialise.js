b0823b4b0a1a19812123bf77b64e907132766d72
Garath Davis <garath.davis.gti@gmail.com>
2018-09-30 02:56:53 +0000

    Did alot of CSS stuff looks fairly ok but still need work on pop up modals

--- assets/js/initialise.js
+++ assets/js/initialise.js
@@ -20,5 +20,15 @@ $(document).ready(function() {
 
     //$("#headlineSearch").hide();
     $("#loading").hide();
-    
+
+    var scrollTop = $(".scrollTop");
+
+    $(scrollTop).click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+
+    });
+
 });
